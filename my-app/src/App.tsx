cd61022c84a003cfe051293b44e7e8ecc29b731c
saloni kael <saloni.kale@codesquaretech.com>
2023-02-21 14:04:39 +0530

    code setup done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="bg-info"> Hey   </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-   
+
     </div>
   );
 }
