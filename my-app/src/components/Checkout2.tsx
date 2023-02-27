a940274d1a9469ed9d05f5bf0ad0c849f42e4cb9
sandeep-csqt <sandeep.vishwakarma@codesquaretech.com>
2023-02-27 11:18:06 +0530

    added checkout page with api call

--- my-app/src/components/Checkout2.tsx
+++ my-app/src/components/Checkout2.tsx
@@ -2,7 +2,7 @@ import React,{ useEffect, useState } from 'react'
 
 interface FormValues {
   name: string;
-  pincode:number,
+  email: string;
   address: string;
   cardNumber: string;
   expiryDate: string;
@@ -12,18 +12,16 @@ interface FormValues {
 const CheckoutForm: React.FC = () => {
   const [formValues, setFormValues] = useState<FormValues>({
     name: "",
-    pincode:0,
+    email: "",
     address: "",
     cardNumber: "",
     expiryDate: "",
     cvv: "",
   });
 
-  const [cartProducts, setCartProducts]= useState(localStorage.getItem('basket') ||"null")
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // console.log(formValues);
+    console.log(formValues);
     // Perform form submission logic here
   };
   
@@ -35,61 +33,84 @@ const CheckoutForm: React.FC = () => {
     setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
   };
 
-  // console.log(formValues,"formValues")
-
+  console.log(formValues,"formValues")
 
   //shiping method api call
-  const [shippingData, setShippingData] = useState([]);
+  const [productData, setProductData] = useState([]);
  
-  let API: string = `https://ecommbackend-yvqe.onrender.com/api/getShippingMethod`;
+  let API: string = `http://192.168.1.210:4000/api/getShippingMethod`;
+console.log(productData,"shiping")
 
   const fetchApiData = async (API: string) => {
+      try{
           const res = await fetch(API);
           const data = await res.json();
-          // console.log(data,"shipin data")
-          setShippingData(data?.data)
+          console.log(data,"shipin data")
+          setProductData(data);
+
+      }
+      catch(error){
+          console.log(error);
+      }
   }
   useEffect(()=>{
       fetchApiData(API);
   },[]);
+  interface ShippingMethod {
+  id: string;
+  name: string;
+  price: number;
+}
+  interface ShippingMethodSelectProps {
+    shippingMethods: ShippingMethod[];
+    selectedShippingMethodId: string | null;
+    onShippingMethodSelect: (id: string | null) => void;
+  }
+  const renderShippingMethodOptions = (shippingMethods: ShippingMethod[]) =>
+  shippingMethods.map((method: ShippingMethod) => (
+    <option key={method.id} value={method.id}>
+      {method.name} (${method.price})
+    </option>
+  ));
 
-  const [shippingMethod, setShippingMethod] = useState<any>();
+  const ShippingMethodSelect = (props: ShippingMethodSelectProps) => (
+    <select
+      value={props.selectedShippingMethodId || ""}
+      onChange={(event) => {props.onShippingMethodSelect(event.target.value || null); console.log(event.target.value)}}
+    >
+      <option value="">-- Select a Shipping Method --</option>
+      {renderShippingMethodOptions(props.shippingMethods)}
+    </select>
+  );
+  const shippingMethods: ShippingMethod[] = [
+    {id: '63f72bb9aa6fc401ddde4f59', name: 'Free', price: 0},
+    {id: '63f72bcfaa6fc401ddde4f5b', name: 'express- in 2 day', price: 99},
+    {id: '63f87d39c289cc0b0b35ad31', name: '7 day delivery', price: 19}
+  ];
+  const [selectedShippingMethodId, setSelectedShippingMethodId] = useState<string | null>(null);
 
-console.log(cartProducts,"cartProducts")
+
   const handleOrder = (e:any) => {
-    console.log('order confirm')
-  if(shippingMethod && cartProducts && formValues?.name  && formValues?.address && formValues?.cardNumber && formValues?.cvv && formValues?.expiryDate){
-
-    console.log("init")
     var data = {
-      orderItems : cartProducts,
+      orderItems : localStorage.getItem(''),
       shippingAddress: {
-        address: formValues?.address,
-        postalCode: formValues?.pincode,
+        address: "sdfsdfsd",
+        postalCode: 1234,
       },
       payment: {
-        name:formValues?.name,
-        cardNumber:formValues?.cardNumber,
+        name:"sbi",
+        cardNumber:111111111,
         type:"credit card",
-        cvv:formValues?.cvv,
-        expiry:formValues?.expiryDate
+        cvv:123,
+        expiry:"1980-01-01"
       },
-      shippingMethod:shippingMethod?.name,
-      shippingPrice: shippingMethod?.price
+      shippingMethod:"Free",
+      shippingPrice: 23
     }
 
-    console.log(data,"data  fetch method herer")
-  }
   }
 
-  const shippingMehtodHandler = (e: any) => {
-    // console.log(e.target.value);
-   shippingData.map((element:any)=>{
-      if(element._id == e.target.value){
-        setShippingMethod(element)
-      }
-    })
-  }
+  {console.log(selectedShippingMethodId)}
 
   return (
     <div className="maincontainer">
@@ -108,6 +129,17 @@ console.log(cartProducts,"cartProducts")
           onChange={handleChange}
         />
       </div>
+      <div className="input-group-prepend p-3">
+        <label htmlFor="email">Email:</label>
+        <input
+         className="form-control"
+          type="email"
+          id="email"
+          name="email"
+          value={formValues.email}
+          onChange={handleChange}
+        />
+      </div>
       <div  className="input-group-prepend p-3" >
         <label htmlFor="address">Address:</label>
         <textarea
@@ -115,17 +147,6 @@ console.log(cartProducts,"cartProducts")
           id="address"
           name="address"
           value={formValues.address}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="input-group-prepend p-3">
-        <label htmlFor="pincode">PinCode:</label>
-        <input
-         className="form-control"
-          type="number"
-          id="pincode"
-          name="pincode"
-          value={formValues.pincode}
           onChange={handleChange}
         />
       </div>
@@ -165,15 +186,12 @@ console.log(cartProducts,"cartProducts")
       <button type="submit" className="btn btn-primary" onClick={(e)=>handleOrder(e)}>Place Order</button>
     </form>
     </div>
-    <div className="col-5 text-center">
-      <h4>Select shipping methods</h4>
-    <select onChange={(e)=>shippingMehtodHandler(e)}>
-      <option>Choose shipping method</option>
-      {shippingData?.map((item:any)=>(
-      <option value={item?._id}>{item?.name} {`(₹ ${item?.price})`}</option>
-      ))}
-    </select> <div>
-   
+    <div className="col-5"> <div>
+      <ShippingMethodSelect
+        shippingMethods={shippingMethods}
+        selectedShippingMethodId={selectedShippingMethodId}
+        onShippingMethodSelect={setSelectedShippingMethodId}
+      />
     </div></div>
     </div>
     </div>
