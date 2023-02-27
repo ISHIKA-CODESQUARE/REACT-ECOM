bc5ddc6c598a8d57ee0fd168c184d9739c33a3f0
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2023-02-27 16:35:53 +0530

    discover more banner redirection

--- my-app/src/components/Home/DiscoverMore.tsx
+++ my-app/src/components/Home/DiscoverMore.tsx
@@ -7,7 +7,7 @@ function DiscoverMore() {
     return (
         <div className='mt-5'>
             <h4 className='mt-5'>Discover more </h4>
-            <img src={fashion8} className="discoverMoreBanner mt-5 mb-5" alt="..." />
+            <a href='/ProductListPage/womens'><img src={fashion8} className="discoverMoreBanner mt-5 mb-5" alt="..." /></a>
        </div>
     )
 }
