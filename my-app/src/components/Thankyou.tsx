658e532035201abced53f3a4dc14c84e1222f0d7
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2023-03-07 17:31:13 +0530

    thank you

--- my-app/src/components/Thankyou.tsx
+++ my-app/src/components/Thankyou.tsx
@@ -9,7 +9,7 @@ type ThankYouProps = {
 const ThankYouPage: React.FC<ThankYouProps> = ({ name, orderId }) => {
   return (
     <div className="container text-center mt-5">
-      <h1>Thank You for Your Purchase!</h1>
+      <h1 className="text-dark">Thank You for Your Purchase!</h1>
       <p>Your order  has been placed successfully and will be shipped soon.</p>
       <p>Thank you for choosing our store!</p>
       <button className="btn btn-success mb-5"><Link to={'/'}>Continue Shopping</Link></button>
