d01db5cb9e8836e94799fb8595f9d848a15d1f2f
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2023-03-07 16:09:31 +0530

    top seller changes

--- my-app/src/components/Home/TopSeller.tsx
+++ my-app/src/components/Home/TopSeller.tsx
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './CSS/Topseller.css';
+import { Container } from 'react-bootstrap';
 
 
 function TopSeller(props: any) {
@@ -11,39 +12,34 @@ function TopSeller(props: any) {
 
 
   return (
-
+<>
     <div className="tiles row card-group mt-5">
       <h1 className='mb-5 text-dark text-center'>Top month Sellers</h1>
-
-      <Row>
-
+    </div>
+      <Container className='ml-5'>
+      <Row >
         {
           productdata?.map((product: any, index: number) => {
-         
-
             return (
               index > 8  && index < 17 ?
               <Col className = "topSeller" lg={3}>
+                <div className='mt-5 mb-5 w-75'>
                 <div>
                   <Link className="nav-link" to={`/productdetail/${product?._id}`}>
 
-                    <img src={`https://ecommbackend-yvqe.onrender.com/${product?.Image}`} className="topImage" alt="..." /> </Link>
+                    <img width={200} src={`https://ecommbackend-yvqe.onrender.com/${product?.Image}`} className="topImage" alt="..." /> </Link>
                 </div>
-                <h5 className="card-title">{product?.Name}</h5>
-                <p className="card-text">{product?.Description}</p>
-                <p className="card-text"><small className="text-muted">Price = {product?.Price}</small></p>
-
+                <h5 className="card-title description">{product?.Name}</h5>
+                <p className="card-text description">{product?.Description}</p>
+                {/* <p className="card-text text-dark fw-bolder description">{product?.Price}</p> */}
+                </div>
               </Col>:""
-
             )
           }) }
-
       </Row>
-
-    </div>
-
+      </Container>
+      </>
   );
-
 }
 
 export default TopSeller
