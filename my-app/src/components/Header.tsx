e79ff339337e1f3dca04dd97b959f788b5386e6c
Divyansh-Csqt <divyansh.bajpai@codesquaretech.com>
2023-03-07 16:29:09 +0530

    navandhome

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -69,7 +69,7 @@ const Header: React.FC = () => {
                   className="collapse navbar-collapse"
                   id="navbarNavAltMarkup"
                 >
-                  <div className="navbar-nav" style={{marginLeft: "9rem"}}>
+                  <div className="navbar-nav" style={{marginLeft: "11rem"}}>
                     <Link className="nav-link" to={"/"}>
                       Home
                     </Link>
@@ -84,9 +84,6 @@ const Header: React.FC = () => {
                         Kids
                       </NavDropdown.Item>
                     </NavDropdown>
-                    <Link className="nav-link" to={"/shop"}>
-                      Shop
-                    </Link>
                     {!state ? (
                       <div className="abc">
                         <Link className="nav-link" to={"/register"}>
